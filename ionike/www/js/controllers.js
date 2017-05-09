5cc87cca31d81fa2c1e8bd565d8d6a6cb1ca0d1f
Andrew Ty Nguyen <andyt.nguyen123@gmail.com>
2017-05-09 16:31:21 -0600

    Add Comment Section

--- ionike/www/js/controllers.js
+++ ionike/www/js/controllers.js
@@ -2,7 +2,7 @@ angular.module('starter.controllers', [])
 
 .controller('FeedController', function($scope, $ionicActionSheet,$timeout, $ionicModal) {
 
-		$scope.liked = false;
+		$scope.liked;
 		$scope.heartRed = function(){
 			$scope.liked = !$scope.liked;
 			console.log($scope.liked);
@@ -38,8 +38,10 @@ angular.module('starter.controllers', [])
 
  //Comments Below
  	$scope.comments = [
-		{name:'John Long Silver', message:'That was so sick', thumb:'http://vignette3.wikia.nocookie.net/pirates/images/7/70/Jack_Sparrow.JPG/revision/latest?cb=20121118005724'},
-		{name: 'Oprah', message: 'Free skateboards for everyone', thumb:'http://static.oprah.com/images/tows/201104/20110413-oprah-whiteshirt-600x411.jpg'}
+		{name:'John Long Silver', message:'That was so sick', thumb:'http://vignette3.wikia.nocookie.net/pirates/images/7/70/Jack_Sparrow.JPG/revision/latest?cb=20121118005724',liked:true},
+		{name: 'Oprah', message: 'Free skateboards for everyone', thumb:'http://static.oprah.com/images/tows/201104/20110413-oprah-whiteshirt-600x411.jpg',liked:true},
+		{name: 'Nanny Huff', message:"You dont know anyone named Johnny Hopkins", thumb:"http://static.wixstatic.com/media/50f3f12a1bbffa6f2b889b1906df9473.wix_mp_512",liked:true},
+		{name:"Brenan Huff", message:"It was Johnny Hopkins, and Sloan Kettering. And they were blazin' that s**t up every day.", thumb:"https://lh3.googleusercontent.com/-LUhmTbiQb-0/Uq6-8Irs_8I/AAAAAAAAAB0/d9eOqIYSJfI/s448-p/photo.jpg",liked:true}
 	];
 
 
@@ -47,14 +49,13 @@ angular.module('starter.controllers', [])
 		$scope.comments.push({
 			name: "Anonymous",
 			message: userMessage.message,
-			thumb:"http://www.shannonillinois.com/wp-content/uploads/2015/11/default_staffmale.jpg"
+			thumb:"http://www.shannonillinois.com/wp-content/uploads/2015/11/default_staffmale.jpg",
 		});
 
+		console.log($scope.comments);
 		$scope.modal.hide();
-
-
 		console.log($scope.userMessage);
-	}
+	};
 
 	  $ionicModal.fromTemplateUrl('templates/modal.html', {
 	    scope: $scope,
